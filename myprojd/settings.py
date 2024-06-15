93062e8cab124f6ca15257cf1c966a0763a4125e
PrajwalK44 <kulkarniprajwal543@gmail.com>
2024-06-15 10:11:38 +0530

    second commit

--- myprojd/settings.py
+++ myprojd/settings.py
@@ -121,4 +121,4 @@ STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
 STATICFILES_DIRS=os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', 'static')
